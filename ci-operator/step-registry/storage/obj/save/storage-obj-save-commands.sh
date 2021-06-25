627f34b7cc48fc604f27d82c987052a9a22fa231
Jan Safranek <jsafrane@redhat.com>
2021-06-25 10:46:09 +0200

    Add a fake junit file
    A fake junit file with a tests failed + passed should mark the test as
    flake, not as failure.

--- ci-operator/step-registry/storage/obj/save/storage-obj-save-commands.sh
+++ ci-operator/step-registry/storage/obj/save/storage-obj-save-commands.sh
@@ -7,6 +7,31 @@ ARTIFACT_DIR=${ARTIFACT_DIR:-/tmp}
 SAVE_FILE=${SHARED_DIR}/initial-objects
 STORAGE_OBJECTS=pv,csidriver,storageclass
 
+function report_flake() {
+    # Create a junit file with a flake, not a failure
+    MESSAGE="$1"
+    STDOUT="$2"
+    mkdir -p ${ARTIFACT_DIR}/junit/
+    cat << EOF > ${ARTIFACT_DIR}/junit/junit_check.xml
+<?xml version="1.0"?>
+<testsuite name="$0" tests="2" skipped="0" failures="1" time="1">
+  <testcase name="$0" time="0">
+    <failure message="">
+    $1
+    </failure>
+    <system-out>
+    $2
+    </system-out>
+  </testcase>
+  <testcase name="$0" time="0"/>
+</testsuite>
+EOF
+    # For build-log.txt
+    echo $MESSAGE
+    echo $STDOUT
+}
+
+
 # Try only 10 times, just in case a cluster setup failed.
 for i in `seq 10`; do
     echo "Attempt $i"
